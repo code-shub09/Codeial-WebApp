f1cda83033a3b0db2095b87ba6c7ddc3235bfb7c
code-shub09 <singhshubham6135@gmail.com>
2024-03-01 02:02:47 +0530

    created a view for home

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 
 const app=express();
 const port=4200;
+const path=require('path');
 
 
 // set up the view engine
-app.use('view engine','ejs');
-app.set('views','./Views');
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+
+
+
 
 /* Mount Router Middleware: Uses app.use() to mount the router middleware defined in the ./routes/index.js file. 
 This means that any requests received by the server will be passed through the routes defined in the index.js router module. */
