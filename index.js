563f3fcd6d9ad840f6d9de3a9e5190187955100b
code-shub09 <singhshubham6135@gmail.com>
2024-03-02 03:22:38 +0530

    static file for indivual views with layout

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const express = require('express');
 const app=express();
 const port=4200;
 const path=require('path');
+
 const expressLayouts= require('express-ejs-layouts');
 
 
 // using expressLayouts before the routes
+app.use(express.static('./assests'));
 app.use(expressLayouts);
 
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 
 // set up the view engine
 app.set('view engine','ejs');
